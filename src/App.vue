09615698d1a61227bdd890cb6419a4963b33b3a5
steele232 <jonathan.steele232@gmail.com>
2020-12-22 17:50:48 -0700

    Added <Hello name="X"/> component
    	modified:   src/App.vue
    	new file:   src/components/Hello.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.JS SICK-App"/>
+    <Hello name="Jonathan"/>
+    <Hello name="Timothy"/>
+    <Hello name="Mom"/>
+    <Hello name="Sara"/>
+    <h1>SOMETHING OBVIOUS!!</h1>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Hello from './components/Hello.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Hello
   }
 }
 </script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cool-name">
+    <h3>{{ name }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style>
+
+.cool-name {
+  background-color: black;
+  color: white;
+}
+
+</style>
+
+
