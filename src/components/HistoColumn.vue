f3426e71cd8f81da547f5ab709837780342537ae
steele232 <jonathan.steele232@gmail.com>
2021-01-03 21:00:19 -0800

    Working! Did table instead of bar graph...
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/components/HistoColumn.vue
    	modified:   src/components/Histogram.vue

---
+++ src/components/HistoColumn.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <h2 :class="[experimental >= theoretical ? 'green' : 'red']"> 
+    {{ experimental.toFixed(2) }}
+  </h2>
+</template>
+
+<script>
+export default {
+  name: 'HistoColumn',
+  props: {
+    theoretical: Number,
+    experimental: Number
+  },
+  data: () => {
+    return {};
+  },
+  methods: {
+    get_percent_string: (Probability) => {
+      let percent = Probability * 100
+      return percent.toString + '%'
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+
+<style lang="less">
+
+* {
+  box-sizing: border-box;
+}
+
+.green {
+  background-color: #45e845;
+}
+
+.red {
+  background-color: #ff9393;
+}
+
+</style>
+
+
+
+
--- src/components/Histogram.vue
+++ src/components/Histogram.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
-    <span v-for="(num, index) in possible_rolls" :key="index">
-      <span class="column">{{all_the_ones()}}</span>
-    </span>
+    <table>
+      <tr>
+        <td>Roll #: </td>
+        <th v-for="(num, index) in possible_rolls" :key="index">
+          <h1>{{ num }}</h1>
+        </th>
+      </tr>
+      <tr>
+        <td>Theoretical Probability: </td>
+        <td v-for="(num, index) in possible_rolls" :key="index">
+          <h2>{{ get_theoretical(num).toFixed(2) }}</h2>
+        </td>
+      </tr>
+      <tr>
+        <td>Experimental Probability: </td>
+        <td v-for="(num, index) in possible_rolls" :key="index">
+          <HistoColumn 
+            :theoretical="get_theoretical(num)" 
+            :experimental="get_experimental(nums, num) || 0"
+          />
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 
 <script>
+import HistoColumn from './HistoColumn.vue'
 
 export default {
   name: 'Histogram',
@@ -20,22 +41,43 @@ export default {
     }
   },
   methods: {
-    all_the_ones: () => {
-      return 5
+    get_theoretical: (Num) => {
+      switch (Num) {
+        case 2: return 1/36;
+        case 3: return 2/36;
+        case 4: return 3/36;
+        case 5: return 4/36;
+        case 6: return 5/36;
+        case 7: return 6/36;
+        case 8: return 5/36;
+        case 9: return 4/36;
+        case 10: return 3/36;
+        case 11: return 2/36;
+        case 12: return 1/36;
+        default: return 0;
+      }
+    },
+    get_experimental: (List, Num) => {
+      let num_all_rolls = List.length
+      let specific_rolls = List.filter(X => X == Num)
+      let num_specific_rolls = specific_rolls.length
+      return num_specific_rolls / num_all_rolls
     }
   },
   components: {
+    HistoColumn
   }
 }
 </script>
 
 
-<style>
+<style lang="less">
 
-.column {
-  padding: 40px;
-  background-color: #EAEAFF
+td, th  {
+  padding: 13px;
+  border: 1px solid black;
 }
+
 
 </style>
 
